de3a5161d5cbe84ada3b24733a8500f29f8c5e09
harshshah456 <harshshahkhs456@gmail.com>
2017-02-12 11:48:47 -0800

    JSOnParsingListview commit

---
+++ app/src/main/java/com/janvi/hp/jsonparsing/Model.java
@@ -0,0 +1,42 @@
+package com.janvi.hp.jsonparsing;
+
+
+
+public class Model {
+    private int user_id;
+    private int id;
+    private String title;
+    private String  body;
+
+    public int getUseri() {
+        return user_id;
+    }
+
+    public void setUseri(int useri) {
+        this.user_id = useri;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
